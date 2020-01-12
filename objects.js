125e0e5222accdbb8bd4b948f6649a51bed8c2fb
Aiswariya Suresh <suresh.aiswariya912@gmail.com>
2020-01-12 14:16:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = {AJR: 'Three-Thirty'};
 function updatePlaylist(playlist, artist, title){
   return Object.assign(playlist, {[artist]: title })
 }
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist
+  return playlist
+}
