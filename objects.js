5fdf6b3cc070adf34247e9fe31a26257c11e0646
Aiswariya Suresh <suresh.aiswariya912@gmail.com>
2020-01-12 14:15:25 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {AJR: 'Three-Thirty'};
 
 function updatePlaylist(playlist, artist, title){
-  return Object.assign({}, playlist, {[artist]: title })
+  return Object.assign(playlist, {[artist]: title })
 }
